ee24ec225bfeaecb68cf849c97683a0d64707c12
Makram Kamaleddine <makram.kd@gmail.com>
2016-07-08 12:31:03 -0400

    explicit require of application helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
+require 'helpers/application_helper'
+
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
@@ -5,9 +7,9 @@ class ApplicationController < ActionController::Base
 
   def hello
     # get access token first
-    access_token = get_access_token
+    access_token = ApplicationHelper.get_access_token
     # make a quick api request for one image
-    response = make_api_request(access_token,
+    response = ApplicationHelper.make_api_request(access_token,
                                 "/photos?feature=fresh_today&sort=created_at&rpp=1&image_size=3&include_store=store_download&include_states=voted")
     render text: response
   end
