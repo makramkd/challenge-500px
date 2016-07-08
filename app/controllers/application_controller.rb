9b5802d2b000215c44ad5de01066d3b39e91bb9c
Makram Kamaleddine <makram.kd@gmail.com>
2016-07-08 12:28:09 -0400

    make a simple request

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,14 @@
-require 'json'
-
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
   def hello
-    api_key = Rails.application.secrets.five_consumer_key
-    if api_key == nil
-      render text: "no api key found"
-    else
-      render text: api_key
-    end
+    # get access token first
+    access_token = get_access_token
+    # make a quick api request for one image
+    response = make_api_request(access_token,
+                                "/photos?feature=fresh_today&sort=created_at&rpp=1&image_size=3&include_store=store_download&include_states=voted")
+    render text: response
   end
 end
