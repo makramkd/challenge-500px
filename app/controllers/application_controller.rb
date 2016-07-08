4f1ac23eb416a2ba2419e3f4f8cf969b2f0875bc
Makram Kamaleddine <makram.kd@gmail.com>
2016-07-08 09:45:06 -0400

    try to use api keys

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,16 @@
+require 'json'
+
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
   def hello
-    render text: "Hello, world!"
+    api_key = Rails.application.secrets.five_consumer_key
+    if api_key == nil
+      render text: "no api key found"
+    else
+      render text: api_key
+    end
   end
 end
